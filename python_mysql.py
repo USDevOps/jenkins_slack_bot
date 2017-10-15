d1dd86546ebcc4be64fe7b770a0d50d0b5216fdf
vivekgrover1 <vivek271091@gmail.com>
2017-10-15 19:35:22 +0530

    Update python_mysql.py

--- python_mysql.py
+++ python_mysql.py
@@ -16,7 +16,7 @@ import pymysql
 
 def get_status(job_id, user):
     # Open database connection
-    db = pymysql.connect("localhost", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
+    db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
     # Prepare SQL query to INSERT a record into the database.
@@ -32,7 +32,7 @@ def get_status(job_id, user):
     db.close()
 
 def update_status(job_id, user):
-    db = pymysql.connect("localhost", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
+    db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     cursor = db.cursor()
     sql = "update jenkinsbot_job_status set {0}='Approved' where username='{1}'".format(job_id, user)
     try:
@@ -44,7 +44,7 @@ def update_status(job_id, user):
 
 
 def add_user(user):
-    db = pymysql.connect("localhost", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
+    db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     cursor = db.cursor()
     sql = "insert into jenkinsbot_job_status values ('%s','Not Approved','Not Approved','Not Approved')" % user
     try:
