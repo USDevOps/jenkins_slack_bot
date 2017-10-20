3d6a50a9aac0b4c380fe16f04fc572b83d099699
vivekgrover1 <vivek271091@gmail.com>
2017-10-20 17:55:49 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,4 +2,4 @@ create database jenkinsbotdb;
 create user 'jenkinsbot'@'localhost' identified by 'jenkinsbot';
 grant all on jenkinsbotdb.* to 'jenkinsbot' identified by 'jenkinsbot';
 use jenkinsbotdb;
-create table jenkinsbot_job_status ( username varchar(30), job_id_1 varchar(20), job_id_2 varchar(20),job_id_3 varchar(20),PRIMARY KEY(username));
+create table jenkinsbot_job_status ( username varchar(30), approval_status varchar(20),PRIMARY KEY(username));
