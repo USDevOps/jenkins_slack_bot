d35f5e2c193d4340d770eeededb5edceee903fbf
vivekgrover1 <vivek271091@gmail.com>
2017-10-26 17:41:27 +0530

    Update python_mysql.py

--- python_mysql.py
+++ python_mysql.py
@@ -14,13 +14,13 @@
 
 import pymysql
 
-def get_status(job_id, user):
+def get_status(user):
     # Open database connection
     db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
     # Prepare SQL query to INSERT a record into the database.
-    sql = "select {0} from jenkinsbot_job_status where username='{1}'".format(job_id, user)
+    sql = "select approval_status from jenkinsbot_job_status where username='{0}'".format(user)
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
@@ -29,29 +29,34 @@ def get_status(job_id, user):
     except:
         return "Error: unable to fetch data"
     # disconnect from server
-    db.close()
+    finally:
+      db.close()
+      cursor.close()
 
-def update_status(job_id, user):
+def update_status(user):
     db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     cursor = db.cursor()
-    sql = "update jenkinsbot_job_status set {0}='Approved' where username='{1}'".format(job_id, user)
+    sql = "update jenkinsbot_job_status set approval_status='Approved' where username='{0}'".format(user)
     try:
         cursor.execute(sql)
         db.commit()
     except:
         db.rollback()
-    db.close()
-
+    finally:
+      db.close()
+      cursor.close()
 
 def add_user(user):
     db = pymysql.connect("db", "jenkinsbot", "jenkinsbot", "jenkinsbotdb")
     cursor = db.cursor()
-    sql = "insert into jenkinsbot_job_status values ('%s','Not Approved','Not Approved','Not Approved')" % user
+    sql = "insert into jenkinsbot_job_status values ('%s','Not Approved')" % user
     try:
         cursor.execute(sql)
         db.commit()
 
     except:
         db.rollback()
-    db.close()
+    finally:
+      db.close()
+      cursor.close()
 ## File : python_mysql.py ends
