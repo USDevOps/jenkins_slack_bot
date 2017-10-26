26f9d694afa02c67ce2d59fecc1988cdb45df148
vivekgrover1 <vivek271091@gmail.com>
2017-10-26 17:48:01 +0530

    Update start_app.py

--- start_app.py
+++ start_app.py
@@ -43,25 +43,22 @@ def message_actions():
     callback = form_json["callback_id"]
     username = form_json["user"]["name"]
     user_id = callback.split('_')[0]
-    job_id = callback.split('_')[1]
-    user_chan_id = callback.split('_')[2]
+    user_chan_id = callback.split('_')[1]
 
     if selection == "Yes":
-        mesg = "Your request has been sent to the Admin for the Approval of job_id_{0}.".format(job_id)
+        mesg = "Your request has been sent to the Admin for the Approval"
         if user_id == username:
             slack_message.update_message(chan_id, msg_ts, mesg)
             slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
             SLACK_NAME = os.environ.get('APPROVER_SLACK_NAME')
             userid = slackbot.get_bot_id(SLACK_NAME, slack_client)
             im_id = slackbot.get_im_id(userid, slack_client)
-            slack_message.send_interactive_message(username, job_id, im_id, user_chan_id)
+            slack_message.send_interactive_message(username, im_id, user_chan_id)
 
     elif selection == "bad":
         mesg = "You choose not to send your request to Admin for Approval."
         if user_id == username:
             slack_message.update_message(chan_id, msg_ts, mesg)
-
-
 
     elif selection == "Not Approved":
         mesg = "Thanks, I will inform the user!"
@@ -73,15 +70,27 @@ def message_actions():
     elif selection == "Approve":
         mesg = "Thanks, I will inform the user!"
         slack_message.update_message(chan_id, msg_ts, mesg)
-        job = "job_id_" + job_id
-        python_mysql.update_status(job, user_id)
-        mesg2 = "Your Request has been approved for {0} now you can execute the command".format(job)
+        python_mysql.update_status(user_id)
+        mesg2 = "Your Request has been approved now you can execute the command"
         slack_message.send_message_without_button(user_id, mesg2, user_chan_id)
 
     return make_response("", 200)
 
 
 if __name__ == "__main__":
+
+    if os.environ.get('SLACK_BOT_TOKEN') is None:
+        print ("SLACK_BOT_TOKEN env variable is not defined")
+    elif os.environ.get('CHATBOT_NAME') is None:
+        print ("CHATBOT_NAME env variable is not defined")
+    elif os.environ.get('APPROVER_SLACK_NAME') is None:
+        print ("APPROVER_SLACK_NAME env variable is not defined")
+    elif os.environ.get('JENKINS_URL') is None:
+        print ("JENKINS_URL env variable is not defined")
+    elif os.environ.get('JENKINS_USER') is None:
+        print ("JENKINS_USER env variable is not defined")
+    elif os.environ.get('JENKINS_PASS') is None:
+        print ("JENKINS_PASS env variable is not defined")
     slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     app.run(host='0.0.0.0', port='80')
 
