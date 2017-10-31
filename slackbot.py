2dd021495e32debb676fdceb27718e3aa00c5b6e
vivekgrover1 <vivek271091@gmail.com>
2017-10-31 17:01:02 +0530

    Update slackbot.py

--- slackbot.py
+++ slackbot.py
@@ -20,6 +20,7 @@ import threading
 import python_mysql
 import slack_message
 
+
 # return username of the user
 def get_user_name(username, slack_client):
     api_call = slack_client.api_call("users.list")
@@ -112,12 +113,12 @@ def parse_slack_output(slack_rtm_output):
 
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text'] and 'thread_ts' in output:
+            if output and 'text' in output and AT_BOT in output['text'].lower() and 'thread_ts' in output:
                 # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['text'].lower().split(AT_BOT)[1].strip().lower(), \
                        output['channel'], output['thread_ts'], output['user']
-            elif output and 'text' in output and AT_BOT in output['text']:
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+            elif output and 'text' in output and AT_BOT in output['text'].lower():
+                return output['text'].lower().split(AT_BOT)[1].strip().lower(), \
                        output['channel'], "Thread_False", output['user']
             elif output and 'type' in output and 'member_joined_channel' in output['type']:
                 return "member joined", output['channel'], "Thread_False", output['user']
@@ -150,11 +151,11 @@ if __name__ == "__main__":
         print ("JENKINS_USER env variable is not defined")
     elif os.environ.get('JENKINS_PASS') is None:
         print ("JENKINS_PASS env variable is not defined")
-        
+
     slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
     BOT_NAME = os.environ.get('CHATBOT_NAME')
     BOT_ID = get_bot_id(BOT_NAME, slack_client)
-    AT_BOT = "<@" + BOT_ID + ">"
+    AT_BOT= "!jenkinsbot"
     threads = []
 
     WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
@@ -163,9 +164,12 @@ if __name__ == "__main__":
         while True:
             sc = slack_client.rtm_read()
             command, channel, msg_id, user_id = parse_slack_output(sc)
+            if command == "":
+               command = "None"
             if command and channel and msg_id and user_id:
                 process_slack_output(command, channel, msg_id, user_id)
             time.sleep(WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
+
 ## File : slackbot.py ends
